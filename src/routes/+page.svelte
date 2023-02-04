2afb28d7eac596029a8229395fe223c6abef79d8
newkincode <maainnewkin59@gmail.com>
2023-02-04 13:20:50 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <h1>newkini-dev.com</h1>
 <p>제작중인 프로그램 또는 게임</p><br>
-<a href="/sfg">SFG - 간단한 농사게임</a><br>
-<p>제작예정 프로그램 또는 게임</p><br>
-<p>city - 현실게임</p><br>
-<p>myClicker - 클리커게임</p><br>
-<p>piefs - piefs is exe file stater</p><br>
-<p>nkalculator - 계산기</p>
+<ul>
+    <li><a href="/sfg">SFG - 간단한 농사게임</a></li>
+</ul>
+<ul>
+    <li><p>제작예정 프로그램 또는 게임</p></li>
+    <li><p>city - 현실게임</p></li>
+    <li><p>myClicker - 클리커게임</p></li>
+    <li><p>piefs - piefs is exe file stater</p></li>
+    <li><p>nkalculator - 계산기</p></li>
+    <li>ORACLE</li>
+</ul>
