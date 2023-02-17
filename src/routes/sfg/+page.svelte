b3b081ef2130333253a27906f24ee8234bbd6ac8
Gongback <94734506+newkincode@users.noreply.github.com>
2023-02-18 01:01:59 +0900

    Update +page.svelte

--- src/routes/sfg/+page.svelte
+++ src/routes/sfg/+page.svelte
@@ -1,3 +1,10 @@
 <title>sfg</title>
+<head>
+    <meta content="newkini dev!" property="og:title" />
+<meta content="sfg다운로드 페이지입니다." property="og:description" />
+<meta content="https://newkini-dev.com/sfg" property="og:url" />
+<meta content="https://github.com/newkincode/simple_farming_game/blob/main/assets/img/icon-512.png?raw=true" property="og:image" />
+<meta content="#c22a0f" data-react-helmet="true" name="theme-color" />
+</head>
 <h1>Simple Farming Game 다운로드!</h1>
 <a href="https://github.com/newkincode/simple_farming_game/releases/download/alpha/sfg-v1.1.1-windows.zip"><button>다운로드</button></a>
