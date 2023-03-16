938f890c91342217aaf3680721dc4c0ad74e5c2b
newkincode <maainnewkin59@gmail.com>
2023-03-16 20:30:25 +0900

    dsfsdsdfadsfgdfgssdgsdfgfsdadfgs

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<head>
+    <meta content="newkini dev!" property="og:title" />
+<meta content="newkini의 사이트입니다." property="og:description" />
+<meta content="https://newkini-dev.com" property="og:url" />
+<meta content="https://avatars.githubusercontent.com/u/94734506?v=4" property="og:image" />
+<meta content="#c22a0f" data-react-helmet="true" name="theme-color" />
+</head>
+<title>메인 사이트</title>
+<h1>newkini-dev.com</h1>
+<p>제작중인 프로그램 또는 게임</p>
+<ul>
+    <li><a href="/sfg">SFG - 모든걸 농사짓자!</a></li>
+    <li><a href="/sfg2">SFG2 - 모든걸 생산하자!</a></li>
+</ul>
+<p>제작예정 프로그램 또는 게임</p>
+<ul>
+    <li><p>city - 현실게임</p></li>
+    <li><p>myClicker - 클리커게임</p></li>
+    <li><p>hapchigi  - 모들걸 합쳐서 만들자!</p></li>
+    <li><p>makingMoney - 일해서 돈벌자</p></li>
+    <li><p>piefs(파이프스) - piefs is exe file stater</p></li>
+    <li><p>nkalculator - 계산기</p></li>
+    <li><p>gg! - gitgui</p></li>
+    <li><p>codetore - code+history  버전관리프로그램</p></li>
+    <li><p>TtsSongMaker - tts로 만든 음성을 붙이고 늘리고 음정바꾸고 등등을 해서 노래를 만드는 프로그램</p></li>
+</ul>
---
+++ src/routes/sfg/+page.svelte
@@ -0,0 +1,13 @@
+<title>sfg</title>
+<head>
+    <meta content="newkini dev!" property="og:title" />
+<meta content="sfg페이지입니다." property="og:description" />
+<meta content="https://newkini-dev.com/sfg" property="og:url" />
+<meta content="https://github.com/newkincode/simple_farming_game/blob/main/assets/img/icon-512.png?raw=true" property="og:image" />
+<meta content="#c22a0f" data-react-helmet="true" name="theme-color" />
+</head>
+<h1>Simple Farming Game 링크!</h1>
+<a href="https://github.com/newkincode/simple_farming_game/releases/download/alpha/sfg-v1.1.1-windows.zip"><button>다운로드(깃헙 릴리즈)</button></a><br>
+<a href="https://github.com/newkincode/simple_farming_game"><button>깃허브</button></a><br>
+<a href="https://newkini.itch.io/simple-farming-game"><button>itch.io</button></a><br>
+<a href="https://discord.gg/TpJPpHwSnM"><button>디스코드</button></a><br>
