368dcf17af032256b5c15066723427fc00855c29
newkincode <maainnewkin59@gmail.com>
2023-04-29 21:23:13 +0900

    no message

--- src/routes/makegame/+page.svelte
+++ src/routes/makegame/+page.svelte
@@ -1,17 +1,19 @@
 <head>
     <meta content="newkini dev!" property="og:title" />
-<meta content="newkini의 사이트입니다." property="og:description" />
-<meta content="https://newkini-dev.com" property="og:url" />
-<meta content="https://avatars.githubusercontent.com/u/94734506?v=4" property="og:image" />
-<meta content="#c22a0f" data-react-helmet="true" name="theme-color" />
+    <meta content="newkini의 사이트입니다." property="og:description" />
+    <meta content="https://newkini-dev.com" property="og:url" />
+    <meta content="https://avatars.githubusercontent.com/u/94734506?v=4" property="og:image" />
+    <meta content="#c22a0f" data-react-helmet="true" name="theme-color" />
+    <title>메인 사이트</title>
 </head>
-<title>메인 사이트</title>
+
 <h1>newkini-dev.com</h1>
 <p>제작중인 프로그램 또는 게임</p>
 <ul>
     <li><a href="/sfg">SFG - 모든걸 농사짓자!</a></li>
     <li><a href="/sfg2">SFG2 - 모든걸 생산하자!</a></li>
     <li><a href="https://github.com/newkincode/TtsSongMaker">TSM - 음성합성 프로그램</a></li>
+    <li><a href="https://github.com/newkincode/inbuff">projectINBUFF - 인공지능 버튜버 인버프</a></li>
 </ul>
 <p>제작예정 프로그램 또는 게임</p>
 <ul>
@@ -19,5 +21,5 @@
     <li><p>myClicker - 클리커게임</p></li>
     <li><p>hapchigi  - 모들걸 합쳐서 만들자!</p></li>
     <li><p>makingMoney - 일해서 돈벌자</p></li>
-    <li><p>projectINBUFF - 인공지능 버튜버 인버프</p></li>
+    
 </ul>
