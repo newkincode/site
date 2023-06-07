99328b6d80d505790021ac2958a7a95165937cfb
newkincode <maainnewkin59@gmail.com>
2023-06-07 21:27:46 +0900

    no message

--- src/routes/sfg/+page.svelte
+++ src/routes/sfg/+page.svelte
@@ -3,12 +3,12 @@
     <meta content="newkini dev!" property="og:title" />
 <meta content="sfg페이지입니다." property="og:description" />
 <meta content="https://newkini-dev.com/sfg" property="og:url" />
-<meta content="https://github.com/newkincode/simple_farming_game/blob/main/assets/img/icon-512.png?raw=true" property="og:image" />
+<meta content="https://github.com/simple-farming-game/simple_farming_game/blob/main/assets/img/icon-512.png?raw=true" property="og:image" />
 <meta content="#c22a0f" data-react-helmet="true" name="theme-color" />
 </head>
 <h1>Simple Farming Game 링크!</h1>
-<a class="btn btn-light" href="https://github.com/newkincode/simple_farming_game/releases/download/alpha/sfg-v1.1.1-windows.zip"><button>다운로드(깃헙 릴리즈)</button></a><br>
-<a class="btn btn-light" href="https://github.com/newkincode/simple_farming_game"><button>깃허브</button></a><br>
+<a class="btn btn-light" href="https://github.com/simple-farming-game/simple_farming_game/releases"><button>다운로드(깃헙 릴리즈)</button></a><br>
+<a class="btn btn-light" href="https://github.com/simple-farming-game/simple_farming_game"><button>깃허브</button></a><br>
 <a class="btn btn-light" href="https://newkini.itch.io/simple-farming-game"><button>itch.io</button></a><br>
 <a class="btn btn-light" href="https://discord.gg/TpJPpHwSnM"><button>디스코드</button></a><br>
 <a class="btn btn-light" href="https://sfg.newkini-dev.com"><button>공식 사이트</button></a><br>
