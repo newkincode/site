1c9cab869b4f22898fc2e53e1d3af227de5a9183
newkincode <maainnewkin59@gmail.com>
2023-06-26 22:11:18 +0900

    ㄹㅣㅇ크 더걸기

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,4 +12,6 @@
 <h1>newkini!</h1>
 <p>My dream is to be a game programmer<br>
 Today is {todayStr}<br></p>
-<a href="https://www.youtube.com/channel/UCa-gBibeaPPiNYl6t_3GOIw?sub_confirmation=1"><button>please subscribe to my youtube channel</button></a>
+<a href="https://www.youtube.com/@newkini-dev?sub_confirmation=1"><button>please subscribe to my youtube channel</button></a><br>
+<a href="https://github.com/newkincode"><button>mygithub</button></a><br>
+<a href="https://discord.gg/cNdPGvCgJh"><button>mydiscord</button></a><br>
