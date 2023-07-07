8561a761e7dbcd311d238f1e53ac0786a151461d
newkincode <maainnewkin59@gmail.com>
2023-07-07 22:17:46 +0900

    커서 생성

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,32 @@
 <script>
 	import Header from './Header.svelte'
 	import './styles.css';
+	import { onMount } from 'svelte';
+
+    onMount(() => {
+        let mouseOut = document.getElementById('mouse-out');
+
+        document.addEventListener('mousemove', (e) => {
+        mouseOut.style.left = mouseOut.style.left = `${e.clientX}px`;
+        mouseOut.style.top = mouseOut.style.top = `${e.clientY}px`;
+        });
+    });
 </script>
 <Header />
 
-	<main>
-		<slot />
-	</main>
+<div id="mouse-out"></div>
+
+
+<slot />
+
+<style>
+    #mouse-out {
+        z-index: 1;
+        backdrop-filter: invert(1);
+        position: absolute;
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        transition: all 500ms cubic-bezier(0.030, 0.425, 0.080, 0.975);
+    }
+</style>
