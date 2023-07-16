ad961892faf691c8b0733de50ad94556557c1718
newkincode <maainnewkin59@gmail.com>
2023-07-16 17:34:01 +0900

    하나 더!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,3 +15,4 @@ Today is {todayStr}<br></p>
 <a href="https://www.youtube.com/@newkini-dev?sub_confirmation=1"><button>please subscribe to my youtube channel</button></a><br>
 <a href="https://github.com/newkincode"><button>mygithub</button></a><br>
 <a href="https://discord.gg/cNdPGvCgJh"><button>mydiscord</button></a><br>
+<a href="https://www.buymeacoffee.com/newkinidev"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a pizza&emoji=🍕&slug=newkinidev&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" /></a>
